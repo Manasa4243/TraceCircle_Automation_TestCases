9a6b0ff311dc6625466f22ec97ee3b24edb1042b
Manasa4243 <manasaj4243@gmail.com>
2026-05-14 11:54:05 +0530

    chrome driver

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,67 +4,87 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class BaseTest {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
 
-    // ✅ URLs
-     
+    // URLs
     protected String LOGIN_URL = "http://localhost:5173/login";
-   protected String ONBOARD_URL = "http://localhost:5173/onboard-super-admin";
-protected String FORGOT_PASSWORD_URL = "http://localhost:5173/forget-password";
-protected String VERIFY_OTP_URL = "http://localhost:5173/verify-otp";
-protected String RESET_PASSWORD_URL = "http://localhost:5173/new-password";
-protected String GTIN_URL = "http://localhost:5173/gtin/add/";
-protected String PLANTS_URL = "http://localhost:5173/plants";
+    protected String ONBOARD_URL = "http://localhost:5173/onboard-super-admin";
+    protected String FORGOT_PASSWORD_URL = "http://localhost:5173/forget-password";
+    protected String VERIFY_OTP_URL = "http://localhost:5173/verify-otp";
+    protected String RESET_PASSWORD_URL = "http://localhost:5173/new-password";
+    protected String GTIN_URL = "http://localhost:5173/gtin/add/";
+    protected String PLANTS_URL = "http://localhost:5173/plants";
+
     @BeforeMethod
     public void setup() {
 
+        // Automatically downloads matching ChromeDriver
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
 
+        // Chrome options for CI/CD
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--window-size=1920,1080");
+
+        // Launch browser
+        driver = new ChromeDriver(options);
+
+        // Maximize browser
         driver.manage().window().maximize();
 
+        // Explicit wait
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
+        // Delete cookies
         driver.manage().deleteAllCookies();
-
-        // ❌ Do NOT open any URL here
-        // Each test class will decide which page to open
     }
 
-    // ✅ Open Login page
+    // Open Login Page
     protected void openLoginPage() {
         driver.get(LOGIN_URL);
     }
 
-    // ✅ Open Onboarding page
+    // Open Onboarding Page
     protected void openOnboardPage() {
         driver.get(ONBOARD_URL);
     }
-        protected void openForgotPasswordPage() {
+
+    // Open Forgot Password Page
+    protected void openForgotPasswordPage() {
         driver.get(FORGOT_PASSWORD_URL);
     }
-    protected void openOTPPage() {
-    driver.get(VERIFY_OTP_URL);
-}
-protected void openResetPasswordPage() {
-    driver.get(RESET_PASSWORD_URL);
-}
-protected void openPlantsPage() {
-    driver.get(PLANTS_URL);
-}
-     @AfterMethod
-    public void tearDown() throws InterruptedException {
 
-        Thread.sleep(5000);
+    // Open OTP Page
+    protected void openOTPPage() {
+        driver.get(VERIFY_OTP_URL);
+    }
+
+    // Open Reset Password Page
+    protected void openResetPasswordPage() {
+        driver.get(RESET_PASSWORD_URL);
+    }
+
+    // Open Plants Page
+    protected void openPlantsPage() {
+        driver.get(PLANTS_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
 
         if (driver != null) {
             driver.quit();
